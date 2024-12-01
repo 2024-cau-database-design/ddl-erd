d8b7d888a5dbc4a052a2b0e7c9252ebb44ae95af
gytks4 <gytks4@naver.com>
2024-12-02 01:16:13 +0900

    upload dml

--- DML.sql
+++ DML.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO user_auth (password_hash, login_id, email) 
 VALUES ('hashed_password', 'owner_login', 'owner_email@example.com');
 
@@ -9,3 +10,15 @@ VALUES ('Sample Restaurant', LAST_INSERT_ID());
 
 INSERT INTO pickup_time (time, restaurant_id) 
 VALUES ('12:00:00', LAST_INSERT_ID());
+
+INSERT INTO `catchtable`.`order_status` (`type`)
+VALUES ('PENDING'), ('COMPLETE');
+
+INSERT INTO `catchtable`.`pickup_status` (`type`)
+VALUES ('BEFORE_PICKUP'), ('AFTER_PICKUP');
+
+INSERT INTO `catchtable`.`reservation_status` (`type`)
+VALUES ('BEFORE_VISIT'),('AFTER_VISIT'),('NO_SHOW');
+
+INSERT INTO `catchtable`.`payment_status` (`type`)
+VALUES ('COMPLETE'),('REFUNDED')
