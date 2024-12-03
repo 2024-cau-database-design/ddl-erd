dafe464d359eb2696ceaecd93948cc1899016271
gytks4 <gytks4@naver.com>
2024-12-03 23:13:38 +0900

    feat: get pickup info list

--- procedure/get_pickup_info_list.sql
+++ procedure/get_pickup_info_list.sql
@@ -1,39 +1,41 @@
-DROP PROCEDURE IF EXISTS get_pickup_info;
+DROP PROCEDURE IF EXISTS get_pickup_info_list;
 
 DELIMITER $$
 
-CREATE PROCEDURE get_pickup_info(IN pickup_id INT)
+CREATE PROCEDURE get_pickup_info_list(IN pickup_ids VARCHAR(1000))
 BEGIN
-    WITH
-    pickup AS (
-        SELECT * FROM pickup WHERE id = pickup_id
-    ),
-    pickup_detail AS (
+    WITH pickup_detail AS (
         SELECT
             p.id AS id,
             p.restaurant_id,
             p.created_at,
             ps.type AS pickup_status,
-            ph.created_at AS updated_at
-            ph.pickup_at AS pickup_at
+            ph.created_at AS updated_at,
+            ph.pickup_at AS pickup_at,
             ph.picked_at AS picked_at
         FROM pickup p
         LEFT JOIN pickup_history ph ON p.id = ph.pickup_id
         LEFT JOIN pickup_status ps ON ph.status_id = ps.id
-        ORDER BY ph.created_at DESC
-        LIMIT 1
+        WHERE FIND_IN_SET(p.id, pickup_ids) > 0
+        AND (p.id, ph.created_at) IN (
+            SELECT pickup_id, MAX(created_at)
+            FROM pickup_history
+            WHERE FIND_IN_SET(pickup_id, pickup_ids) > 0
+            GROUP BY pickup_id
+        )
     ),
-    pickup_and_order as
-     (SELECT
-         pd.*,
-         o.id AS order_id,
-         os.type AS order_status,
-         o.created_at AS order_created_at,
-         o.total_price,
-         o.reservation_fee
-     FROM pickup_detail pd
-     LEFT JOIN `order` o ON pd.id = o.booking_id
-     LEFT JOIN order_status os ON o.status_id = os.id),
+    pickup_and_order AS (
+        SELECT
+            pd.*,
+            o.id AS order_id,
+            os.type AS order_status,
+            o.created_at AS order_created_at,
+            o.total_price,
+            o.reservation_fee
+        FROM pickup_detail pd
+        LEFT JOIN `order` o ON pd.id = o.booking_id
+        LEFT JOIN order_status os ON o.status_id = os.id
+    ),
     order_items AS (
         SELECT
             oi.order_id,
@@ -47,6 +49,7 @@ BEGIN
             ) AS items
         FROM order_item oi
         JOIN restaurant_menu rm ON oi.menu_id = rm.id
+        WHERE oi.order_id IN (SELECT order_id FROM pickup_and_order)
         GROUP BY oi.order_id
     )
     SELECT
@@ -54,12 +57,11 @@ BEGIN
         oi.items AS order_items
     FROM pickup_and_order po
     LEFT JOIN order_items oi ON po.order_id = oi.order_id;
-
 END$$
 
 DELIMITER ;
 
-CALL get_pickup_info(12);
+CALL get_pickup_info_list('15,16');
 
 SELECT * from pickup;
 SELECT * from `order`;
