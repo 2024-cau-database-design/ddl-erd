9c1d15d2e35aec9099fe3d9eebf1e6ece60d2884
gytks4 <gytks4@naver.com>
2024-12-04 10:18:41 +0900

    feat: get_pickup_info_list 에 유저정보 추가

--- procedure/get_pickup_info_list.sql
+++ procedure/get_pickup_info_list.sql
@@ -8,14 +8,20 @@ BEGIN
         SELECT
             p.id AS id,
             p.restaurant_id,
+            p.customer_id,
             p.created_at,
             ps.type AS pickup_status,
             ph.created_at AS updated_at,
             ph.pickup_at AS pickup_at,
-            ph.picked_at AS picked_at
+            ph.picked_at AS picked_at,
+            c.name AS customer_name,
+            c.phone_number AS customer_phone,
+            ua.email AS customer_email
         FROM pickup p
         LEFT JOIN pickup_history ph ON p.id = ph.pickup_id
         LEFT JOIN pickup_status ps ON ph.status_id = ps.id
+        LEFT JOIN customer c ON p.customer_id = c.id
+        LEFT JOIN user_auth ua ON c.id = ua.id
         WHERE FIND_IN_SET(p.id, pickup_ids) > 0
         AND (p.id, ph.created_at) IN (
             SELECT pickup_id, MAX(created_at)
@@ -49,15 +55,31 @@ BEGIN
             ) AS items
         FROM order_item oi
         JOIN restaurant_menu rm ON oi.menu_id = rm.id
-        WHERE oi.order_id IN (SELECT order_id FROM pickup_and_order)
         GROUP BY oi.order_id
     )
-    SELECT
-        po.*,
-        oi.items AS order_items
+    SELECT 
+        po.id,
+        po.restaurant_id,
+        JSON_OBJECT(
+            'id', po.customer_id,
+            'name', po.customer_name,
+            'phoneNumber', po.customer_phone,
+            'email', po.customer_email
+        ) AS customer,
+        po.created_at,
+        po.pickup_status,
+        po.updated_at,
+        po.pickup_at,
+        po.picked_at,
+        po.order_id,
+        po.order_status,
+        po.order_created_at,
+        po.total_price,
+        po.reservation_fee,
+        IFNULL(oi.items, JSON_ARRAY()) as order_items
     FROM pickup_and_order po
     LEFT JOIN order_items oi ON po.order_id = oi.order_id;
-END$$
+END $$
 
 DELIMITER ;
 
